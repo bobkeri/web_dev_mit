4f3564ad7c23292c1a13c3c0038bfa2a59602844
bobkeri <keribacus@gmail.com>
2017-12-08 13:03:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ print hash('sha256', 'Bob keri Bacus');
 print hash('sha256', 'Bob keri Bacus')
 ?>
 <br><br>
-<img src="act.png">  
+  
 <a href="fail.php">Click here to check the error setting </a><br>
 <a href="check.php">Click here to cause a traceback</a>
 </body>
